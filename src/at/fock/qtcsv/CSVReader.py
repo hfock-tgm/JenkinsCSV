d57ebd9159aad5afa2c83bdbca6656c2fdfd3af2
hfock-tgm <hagen.fock@gmail.com>
2016-02-16 09:36:02 +0100

    added tests

---
+++ src/at/fock/qtcsv/CSVReader.py
@@ -0,0 +1,29 @@
+import csv
+class CSVReader():
+    def __init__(self, filename):
+        self.filename = filename
+
+    def loadCSV(self):
+        helpVar = ""
+        with open(self.filename, 'rt') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+            for row in reader:
+                helpVar += ' '.join(row) + "\n"
+        return helpVar
+
+    def saveCSV(self, text):
+        with open(self.filename, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            zeilen = text.split("\n")
+            for zeile in zeilen:
+                if zeile != "":
+                    woerter = zeile.split(" ")
+                    writer.writerow(woerter)
+
+if __name__ == "__main__":
+    print ("test")
+    test = CSVReader('..\CSV\csv_file.csv')
+    test.saveCSV('1;2;3;4;')
+    print test.loadCSV()
+    test.saveCSV('4;3;2;1;')
+    print test.loadCSV()
---
+++ src/at/fock/test/CSVtest.py
@@ -0,0 +1,59 @@
+import unittest
+from at.fock.qtcsv.CSVReader import CSVReader
+
+class CSVtest(unittest.TestCase):
+
+    def setUp(self):
+        self.csv = CSVReader('..\CSV\csv_file.csv')
+
+    def test_load(self):
+        try:
+            self.csv.loadCSV()
+        except IOError as e:
+            self.fail("Where is the CSV-file?\n %s" % e)
+
+    def test_save(self):
+        try:
+            self.csv.saveCSV("")
+        except IOError as e:
+            self.fail("Nope... Didn't work!\n%s" % e)
+
+    def test_saveText(self):
+        try:
+            self.csv.saveCSV("1;2;3;")
+            text = self.csv.loadCSV()
+            assert text == "1;2;3;\n", "Doesn't match OMBRE!"
+        except IOError as e:
+            self.fail("Where is the CSV-file?\n %s" % e)
+
+    def test_addText(self):
+        try:
+            added_text = "4;5;6;"
+            old_text = self.csv.loadCSV()
+            self.csv.saveCSV(old_text+added_text)
+            new_text = self.csv.loadCSV()
+            self.assertTrue(new_text == old_text+added_text+"\n", "Doesn't match OMBRE!")
+        except IOError as e:
+            self.fail("Where is the CSV-file?\n%s" % e)
+
+    def test_linebreaks(self):
+        try:
+            asserted_text = "1;\n2;\n3;"
+            added_text = "1;\n\n\n\n\n2;\n\n\n\n\n3;"
+            old_text = self.csv.loadCSV()
+            self.csv.saveCSV(old_text+added_text)
+            new_text = self.csv.loadCSV()
+            self.assertFalse(new_text == old_text+added_text, "Blanks no save no save")
+            self.assertTrue(new_text == old_text+asserted_text+"\n",   "Blanks no save no save! Ali just cleaning")
+        except IOError as e:
+            self.fail("Where is the CSV-file?\n%s" % e)
+
+    def cleanItUp(self):
+        try:
+            self.csv.saveCSV("")
+        except IOError as e:
+            self.fail("Couldn't clean CSV-file!\n%s" % e)
+
+
+if __name__ == '__main__':
+    unittest.main()
