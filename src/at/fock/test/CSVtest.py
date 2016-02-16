3aabd52e9ee9f953e8eaefc54a93e2611d30865a
hfock-tgm <hagen.fock@gmail.com>
2016-02-16 10:07:28 +0100

    minor changes again and again and again

--- src/at/fock/test/CSVtest.py
+++ src/at/fock/test/CSVtest.py
@@ -1,10 +1,10 @@
 import unittest
-from at.fock.qtcsv import CSVReader
+from at.fock.qtcsv.CSVReader import CSVReader
 
 class CSVtest(unittest.TestCase):
 
     def setUp(self):
-        self.csv = CSVReader.CSVReader('../CSV/csv_file.csv')
+        self.csv = CSVReader('../CSV/dcsv_file.csv')
 
     def test_load(self):
         try:
